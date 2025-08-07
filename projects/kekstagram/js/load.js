6e3f20f36fef78c5dcd6b9a8a85b30fff7a3a871
Egor Limachev <le@sinergium.ru>
2025-08-07 08:55:43 +0300

    Меняем путь закачки данных

--- projects/kekstagram/js/load.js
+++ projects/kekstagram/js/load.js
@@ -1,29 +1,32 @@
-'use strict';
+"use strict";
 (function () {
-  var ADRESS_SERVER_DATA = 'https://25.javascript.pages.academy/kekstagram/data';
+  // Функция не нужна, так как серв закрыт
+  // Берем данные из файла data.js
+  var ADRESS_SERVER_DATA =
+    "https://25.javascript.pages.academy/kekstagram/data";
   var StatusCode = {
-    OK: 200
+    OK: 200,
   };
   var TIMEOUT_IN_MS = 10000;
 
   window.load = function (onSuccess, onError) {
     var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-    xhr.open('GET', ADRESS_SERVER_DATA);
+    xhr.responseType = "json";
+    xhr.open("GET", ADRESS_SERVER_DATA);
 
-    xhr.addEventListener('load', function () {
+    xhr.addEventListener("load", function () {
       if (xhr.status === StatusCode.OK) {
         onSuccess(xhr.response);
       } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+        onError("Статус ответа: " + xhr.status + " " + xhr.statusText);
       }
     });
 
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
+    xhr.addEventListener("error", function () {
+      onError("Произошла ошибка соединения");
     });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    xhr.addEventListener("timeout", function () {
+      onError("Запрос не успел выполниться за " + xhr.timeout + "мс");
     });
     xhr.timeout = TIMEOUT_IN_MS;
 
